de8652dda62dc0ae75fe90e70a3d7eb4c376d4fb
Yoohojin <yoohojin1234@gmail.com>
2022-05-19 11:24:28 +0900

    spring boot application 추가

---
+++ src/main/java/com/hj/placesearchproject/PlaceSearchProjectApplication.java
@@ -0,0 +1,13 @@
+package com.hj.placesearchproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaceSearchProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlaceSearchProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hj/placesearchproject/PlaceSearchProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hj.placesearchproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaceSearchProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
